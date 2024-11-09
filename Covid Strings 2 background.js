37c88e301d68ca04473506b671016920c522fe74
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-08 22:11:51 -0500

    Create Covid Strings 2 background.js
    I can't believe I didn't just copy and paste my background code for version 1 this is so much easier.

---
+++ Covid Strings 2 background.js
@@ -0,0 +1,65 @@
+doc.type HTML
+script
+file name= covid strings version2 background .js 
+java.awt graphics
+img=document.createElement(covid)
+img.src=http://www.google.com/intl/en_com/images/logo_plain.png
+src=document getelementbyiD(header)
+src.AppendChild(img)
+<div id=header></div>
+audio=newAudio(audio_file mp3)
+audio.play 
+jbutton random button
+jbutton (set random color)
+private final j button option button=
+jbutton (change color)
+public fun graphics(){
+jpanel color panel=j panel();
+Add (color.panel, border layout.center)
+jpanel button panel=
+jpanel (flowlayout(flowlayout.center)
+buttonpanel.add(randombutton);
+random.button add actionListener(actionlistener(){
+override 
+public actionperformed(actioneventthirteen){
+random r= newrandom();
+colorpanel.setBackground((rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))
+buttonpanel.add(optionbutton);
+optionbutton.add actionlistener(actionlistener(){
+override 
+public actionperformed(actionevent e){
+color multi=jcolorchooser.showdialog(funwithgraphics.this,))
+If(multi!=Null)
+color panel set.background(m)
+add(buttonpanel,borderlayout.south)
+public static main(string [] args){
+jframe f = covidstrings2()
+f.set title(covid strings2)
+f.setcloseoperation(jframe.exit_on_close);
+f.setsize
+f.setvisible(true);
+<labelid=lblscore>zero</label>
+score=zero
+If (mult == x*y)
+[non-concatenated]
+score+=ten
+Else:
+score-=ten
+If (mult ==x*y)
+[concatenated]
+score+=twenty
+Else:
+score-=twenty
+lblscore=document.getelementbyid(lblscore)
+lbl.score.innerHTML=score
+private j button next button=jbutton(next)
+nextbutton add.actionlistener
+public actionperformed(action event e) 
+system.out print.in (next button clicked)
+frame.add(next button);
+frame set visible true
+object {a:one,b:two,c:three,d:four,e:five,f:six,g:seven,h:eight}
+keys object keys(team)
+entries object entries(obj);
+value =object [team color]
+Return filename 
