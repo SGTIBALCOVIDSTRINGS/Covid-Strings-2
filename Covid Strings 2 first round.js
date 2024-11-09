2dc6dce9c1be83c9b9699cdc34459ffe72afd472
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-08 22:35:24 -0500

    Create Covid Strings 2 first round.js
    I also learned my mistakes from covid strings one first round.

---
+++ Covid Strings 2 first round.js
@@ -0,0 +1,104 @@
+<doc.type HTML>
+<script>
+html lang=en
+head
+utf-eight
+ < Viewport Content=Width=Device-Width,Inital Scale One
+        <Title> Covid Strings2 First Round Bracket</Title>
+        <Link Rel=Stylesheet Href=Styles.CSS
+        </Head>
+        <Body>
+            </Html>
+            Brakcet Container{}
+            Display: Grid;
+            Gap : Tenpx:
+            .Matchup{}
+            Border : Onepx Solid Zero;
+            Padding: Tenpx;
+            Text-Align: Center;
+filename=covid strings2 first round.js
+console.log(covid strings2 first round)
+covid strings first round=(highest overall seed ,second highest seed third highest seed first middle seed, second middle seed,  third lowest seed ,second lowest seed,lowest overall seed)
+highest overall seed = 1 seed
+second highest seed=2 seed
+third highest seed=3 seed
+firstmiddleseed=4seed
+second middle seed = 5 seed
+third lowest seed= 6 seed
+second lowest seed = 7 seed
+lowest overall seed = 8 seed
+teams. foreach((team,index))=>{}
+create matchup (magenta magnetizers versus red protein spikers))
+matchup=document.createelement(div)
+matchup.covid strings first round.add(matchup)
+matchup.textcontent=team
+bracket.container.appendchild(matchup)
+create matchup (blue curve crushers versus white transit angels)
+matchup.covid strings seed list .add(matchup)
+matchup.textcontent=team
+bracket.container.appendchild(matchup)
+create matchup (purple brave hearts versus orange messengers)
+matchup.covid strings seed list.add(matchup)
+matchup.textcontent=team
+bracket.container.appendchild(matchup)
+create matchup (black night riders versus pink covid disease fighters)
+matchup.covid strings seed list.add(matchup)
+matchup.TextContent=Team
+bracket.Container.AppendChild(Matchup)
+teams=[orange rna messengers pink covid disease fighters,red protein spikers,white transit angels, blue curve crushers, magenta magentizers, black night riders purple brave hearts]
+rounds = Math.log2(teams.length);
+currentteams = teams;
+roundDiv =document.createelement(Div);
+roundDiv.ClassName = Covid Strings2 First Round;
+roundDiv.AppenedChild(MatchupDiv)
+teams. ForEach((Team,Index))=>{}
+questions[]Questions={
+collections.Shuffle(Arrays.Aslist(Questions)
+question1 
+question2
+question3
+question4
+question5 
+question6
+question7
+question8
+question9
+question10
+question11
+question12
+question13
+question14
+question15
+question16
+question17 
+private string getblanks(int numberofnlanks)
+char[] chars = char[numberofblanks]
+arrays.fill chars 
+string chars 
+bracket.appendchild(roundDiv)
+declare winner
+result =declareWinner(team1score,team2score);
+advancingteams=currentteams.slice(teams length);
+If red score >=fourty:
+console.log(red protein spikers win )
+Else:
+console.log(magenta magnetizers win)
+result =declareWinner(team1score,team2score);
+advancingteams=currentteams.slice(teams length);
+If white score >=fourty:
+console.log(white transit angels win )
+Else:
+console.log(blue curve crushers Win)
+result =declareWinner(team1score,team2score);
+advancingteams=slice(teams length)
+If orange score >=fourty:
+console.log(orange rna messengers win )
+Else:
+console.log(purple brave hearts win)
+result = declarewinner(team1score, team2score);
+advancingteams=slice(teams.length);
+If pink score >=fourty:
+console.log(pink covid disease fighters win )
+Else:
+console.log(black night riders win)
+Return fileName
