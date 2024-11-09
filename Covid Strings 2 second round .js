239e3440f04d1f6aabca8d11711c9bcdba569258
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-08 22:50:26 -0500

    Create Covid Strings 2 second round .js
    learned the same lesson for the second round.

---
+++ Covid Strings 2 second round .js
@@ -0,0 +1,53 @@
+<doc.type HTML>
+script
+  html
+head
+filename=covidstrings2 second round .js
+highest remaining seed =(highest remaining num value)
+lowest remaining seed = (lowest remaining num value )
+intermediateSeed1=(First Middle Remaining Value In Covid Strings List)
+intermediateSeed2=(Second Middle Remaining Value In Covid Strings List)
+console.log(Covid Strings2 Second Round)
+create Matchup=(intermediate seed2 versus intermediate seed1)
+matchup = document.createelement(div);
+matchup.classList.Add(Matchup);
+matchup.InnerHTML = <Div>${Team1}</Div><Div>Vs</Div><Div>${Team2}</Div>
+Return matchup;
+create matchup=(lowest remaining seed Versus Highest Remaining Seed )
+matchup =document.CreateElement(Div);
+matchup.ClassList.Add(Matchup);
+matchup.InnerHTML = <Div>${Team1}</Div><Div>Vs</Div><Div>${Team2}</Div>
+Import Java Util.Scanner
+Import Java.Util
+questions[]Questions={
+question18
+question19
+question20
+question21
+question22
+question23 
+question24
+question25
+collections.shuffle(arrays.aslist(questions)
+private string getBlanks(int numberofblanks)
+char[] chars = char[NumberofBlanks]
+arrays.fill chars 
+Return string chars 
+AddEventListener( => {
+If (event.target.ClassList.Contains(Team)) {
+declare winner =event.target.TextContent;
+result = declareWinner(team1score,team2score);
+advancingteams=currentteams.slice(currentteams)
+If intermediate seed1 score>=fourty:
+console.log(intermediate seed1 wins)
+Else:
+console.log(intermediate seed2 wins)
+If (event.Target.ClassList.Contains(Team)) {
+declare winner = event.target.textcontent;
+result = declarewinner(team1score,team2score);
+advancingteams=slice(currentteams)
+If highest remaining seed score>=fourty:
+console.log (highest remaining seed wins)
+Else:
+console.log(lowest remaining seed wins)
+Return filename
