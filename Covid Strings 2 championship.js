e240c28647ad8d2d072f274c9261510d90406e38
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-08 22:59:10 -0500

    Create Covid Strings 2 championship.js
    I know there are more edits for this file and I will do those later.

---
+++ Covid Strings 2 championship.js
@@ -0,0 +1,39 @@
+<doc type html> 
+<script>
+html
+head
+filename=covid strings2 championship game .JS  
+team high seed= highest remaining seed
+team low seed = lowest remaining Seed
+championship game=(high versus lowest remaining seed)  
+console.log(covid strings2 championship game)
+rounddiv name=covid strings2 championship round
+matchupdiv = document.createelement(div);
+matchupdiv.classname =matchup;
+matchupdiv.innerHTML =[team low seed versus team high seed]
+rounddiv.appendchild(matchupdiv);
+bracket.appendchild(rounddiv);
+advancingteam =slice(currentteams) 
+java util.scanner
+java.util
+questions[]questions={
+question26
+question27
+question28
+question29
+question30
+question31
+question32
+substring(start,end)
+userin.multiple choice
+If SelectedAnswer==Correct Answer:
+Return True
+Else:
+Return False
+Let Result = DeclareWinner(Team1Score, Team2Score);
+Declare Winner 
+If  Team High Seed Championship Game Score >=Sixty:
+Console.Log (High Seed Wins Covid Strings2) 
+Else:
+Console.Log(Low Seed Wins Covid Strings2 )
+Return FileName
