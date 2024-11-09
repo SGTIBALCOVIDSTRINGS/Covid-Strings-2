88219b6d9dfc2361fd45e9c1db00c04cbf4b2caf
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-09 15:22:42 -0500

    Create Covid strings 2 game code .js
    This game code includes references to immunization versus vaccination. I had a copying error in which the filename when copied said background JS rather than game code JS While some elements of the background are in the overall game code it is not the same file.

---
+++ Covid strings 2 game code .js
@@ -0,0 +1,120 @@
+<doc type.html>
+<script>
+filename=covid Strings2 game code .js 
+console.log(covid strings2 )
+java.awt graphics
+img=document.createelement(covid)
+img.src=http://www.google.com/intl/en_com/images/logo_plain.png
+src=document getelementbyiD(header)
+src.appendchild(img)
+<div id=header></div>
+audio=audio(audio_file mp3)
+audio.play 
+jbutton random button
+jbutton (set random color)
+private final j button option button=
+j button (change color)
+public fun graphics(){
+jpanel color panel=j panel();
+add (color.panel, border layout. center )
+jpanel button panel=
+j panel (flowlayout(flowlayout.center)
+buttonpanel.add(randombutton);
+random.button add actionlistener(actionlistener(){
+override 
+publicactionperformed(actionevent e){
+random r= newrandom();
+colorpanel.setbackground(Rnextint(thirteen),)(Rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))
+buttonpanel.add(optionbutton);
+optionbutton.add actionlistener(action listener(){
+override 
+publicactionperformed(actionevente){
+color multi=jcolorchooser.showdialog(funwithgraphics.this,)
+If(multi!=Null)
+color panel set.background(m)
+Add(buttonPanel, borderlayout.south)
+public static  main(string [] args){
+jframe f = covidstrings2()
+f.set title(covidstrings2)
+f.setcloseOperation(jframe.exit_on_close);
+f.setsize
+f.set visible(true);
+<label id=lblscore>zero</label>
+score=zero
+If (mult == x*y)
+[non-concatenated]
+score+=ten
+Else:
+score-=ten
+If (mult == x*y)
+[concatenated]
+score+=twenty
+Else:
+score-=twenty
+lblScore= document.getelementbyid(lblScore)
+lbl.score.innerHTML=score
+private j button next button=jbutton(next)
+nextbutton add.actionlistener
+public actionperformed(action event e) 
+system.out print.in (next button clicked)
+frame.add(next button);
+frame set visible true
+object {a:one,b:two,c:three,d:four,e:five,f:six,g:seven,h:eight}
+keys object keys(team)
+entries object entries(obj);
+value object= [Team Color]
+covid strings2 team list=[orange,pink,red,white,blue,magenta,black,purple]
+covid strings2 debugging question=(whats wrong With This code the +covid+death+In+terms+Of+pi++is+one thousand) 
+covid strings2 phrase list=[non-concatenated,no sun cubed does not kill viruses, elon musk is wrong take the purple pill, nothing labeled dexie turns you into benjamin button, i am not a superspreader i am a supraspreader mdx does not mean medical doctors prescription,astrazeneca is only slightly better than the saturn astra, talk about staying at home lasts shorter than a soda can on venus aaron rodgers proves the coverup is truly worse than the crime
+Concatenated,no+ corona +beer+has +nothing+to+Do+With+corona+virus,work +from+home+is+not+just+For+rihanna,it+is+also+not+just+fifth+harmonys+right+either,vitamin+d+however+is+a+compliment+to+getting+vaccinated,at+least+sixty+minutes+of+exercise+also+helps,no+covid+was+not+cheated+out+of+the+twenty-twenty+election,one+comedian+believed+the+last+year+was+one+and+four+twentyfirsts+of+a+uranian+year
+it+figures+a+bubble+would+employ+a+round+robin+format]
+covidstringsmultiplechoicelist2=[1: a shorter b longer c a reflection of d the same , 2:a cheated b arrested c stolen d expelled ,3:a people With autism b nfl employees c rihanna d social media influencers, 
+4: a news b supra c ymca d misinformation,5:a supersized b cubed c squared d blocked, 6: a beer b cerveza c solar flare d cider, 7:a blue b purple c red d orange, 8: a doxing poster boy b a meth addict c a social media influencer d benjamin button,
+9:a megan thee stallions b fifth harmonies c ty dolla signs d natasha lyonnes, 10 a one hundred b sixty c eighty d fourty, 11 a cover-up b discount double check c inadequate usage of a first round pick d texans salary cap issues, 
+12:a mimas b astrolabe c astra d rhea, 13: a d b k c special k d c,14: a mdx b crx c mdrx d rlx,15:a it isnt a string b you cant mention a number If youre In num pi c it is not any metric unit d np In pi is different In geomtry than In coding, 
+16:a venusian b martian c neptunian d uranian,17:a bubble b covid playoff c can jam tournament d covid election 18: a brain chemicals b immunity c trump derangement syndrome d covid risk, 
+19: a negative twenty b fifteen c twenty d twenty-two,20:a wealth b fame c power d belief In god,21:a twice b half c four times d just]
+covidstrings2mathproblemlist=[If aaron rodgers decided 2 weeks later that he would get a johnson and johnson vaccine how soon after getting vaccinated can he play?,the local flag day In town has been postponed due to an outbreak of the variant omicron If the guidelines say that the positivity rate can be not greater than six and a quarter percent and the rate is six and four ninths percent can the event happen on the New date of june seventeenth?, If former president trump shared fourteen covid misinformation posts over a period of two days assuming they are divided equally, how many posts per day did trump share?, If the same town decided to have a juneteenth celebration and the same guidelines regarding positivity were used could juneteenth be held If the rate was five and one fiftieth percent?, If a study found that immunization increases the likelihood that one would decline vaccination by sixty-five percent what could get done to convince skeptical people? ,This same study found that when asked about what party they would vote For In the midterms seventy one percent of people who were immunized would vote republican whereas eighty percent of people who say they were vaccinated vote democrat If the margin of error is two and nine tenths percent what is the range?, If+it+becomes+public+that+vitamin+d+actually+only+has+a+third+of+the+efficacy+of+vaccines+should+the+store+offer+vitamin+d?,the+same+poll+showed+that+If+a+New+variant+came+support+would+increase+by+three+percent+what+is+the+New+range?,the+same+poll+showed+that+once+the+variant+leaves+support+recedes+by+five+percent+what+is+the+New+range?]
+covidstrings2sentencelist=[trump+said+covid+made+him+twenty+years+younger,vitamin d exposure is one way to boost immunity ,work from home is not just For famous people,covid+misninformation+spreads+just+as+fast+as+covid+itself
+string.split   
+arrays.aslist(list<string>)
+For(string ss:arr){
+system.outprintin(ss)
+java util.scanner
+java.util
+questions[]questions={
+collections.shuffle(arrays.aslist(questions)
+private string getblanks(int numberofblanks)
+char[] chars =char[numberofblanks]
+arrays.fill chars 
+Return string chars 
+If math problem== x percent:
+subtract exact from one hundred
+If math problem== margin of error:
+add and subtract exact 
+Else:
+remain constant
+user input.select team
+Import meta
+myClass=Class
+constructor(non-concatenated)
+Return category
+If category==math problem:
+prompt
+log
+Else:
+select from multiple choice
+myclass=newmyclass(concatenated) 
+Return category
+If category==math problem:
+prompt
+log
+Else:
+select from multiple choice
+Import syntax 
+substring(start,end)
+If selectedanswer==correct answer:
+Return true
+Else:
+Return false
+Return filename 
