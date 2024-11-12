eb936987bdc2f2ce93836e95d79884c8a78eb0b6
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-11 19:07:57 -0500

    Update Covid strings 2 game code .js
    really all done.

--- Covid strings 2 game code .js
+++ Covid strings 2 game code .js
@@ -30,9 +30,9 @@ optionbutton.add actionlistener(action listener(){
 override 
 publicactionperformed(actionevente){
 color multi=jcolorchooser.showdialog(funwithgraphics.this,)
-If(multi!=Null)
+if(multi!=null)
 color panel set.background(m)
-Add(buttonPanel, borderlayout.south)
+add(buttonPanel, borderlayout.south)
 public static  main(string [] args){
 jframe f = covidstrings2()
 f.set title(covidstrings2)
@@ -66,16 +66,16 @@ value object= [teamcolor]
 covid strings2 team list=[orange,pink,red,white,blue,magenta,black,purple]
 covid strings2 debugging question=(whats wrong with this code the +covid+death+in+terms+of+pi++is+one thousand) 
 covid strings2 phrase list=[non-concatenated,no sun cubed does not kill viruses, elon musk is wrong take the purple pill, nothing labeled dexie turns you into benjamin button, i am not a superspreader i am a supraspreader mdx does not mean medical doctors prescription,astrazeneca is only slightly better than the saturn astra, talk about staying at home lasts shorter than a soda can on venus aaron rodgers proves the coverup is truly worse than the crime
-concatenated,no+ corona +beer+has +nothing+to+Do+With+corona+virus,work +from+home+is+not+just+For+rihanna,it+is+also+not+just+fifth+harmonys+right+either,vitamin+d+however+is+a+compliment+to+getting+vaccinated,at+least+sixty+minutes+of+exercise+also+helps,no+covid+was+not+cheated+out+of+the+twenty-twenty+election,one+comedian+believed+the+last+year+was+one+and+four+twentyfirsts+of+a+uranian+year
+concatenated,no+ corona +beer+has +nothing+to+do+with+corona+virus,work +from+home+is+not+just+for+rihanna,it+is+also+not+just+fifth+harmonys+right+either,vitamin+d+however+is+a+compliment+to+getting+vaccinated,at+least+sixty+minutes+of+exercise+also+helps,no+covid+was+not+cheated+out+of+the+twenty-twenty+election,one+comedian+believed+the+last+year+was+one+and+four+twentyfirsts+of+a+uranian+year
 it+figures+a+bubble+would+employ+a+round+robin+format]
-covidstringsmultiplechoicelist2=[1: a shorter b longer c a reflection of d the same , 2:a cheated b arrested c stolen d expelled ,3:a people With autism b nfl employees c rihanna d social media influencers, 
+covidstringsmultiplechoicelist2=[1: a shorter b longer c a reflection of d the same , 2:a cheated b arrested c stolen d expelled ,3:a people with autism b nfl employees c rihanna d social media influencers, 
 4: a news b supra c ymca d misinformation,5:a supersized b cubed c squared d blocked, 6: a beer b cerveza c solar flare d cider, 7:a blue b purple c red d orange, 8: a doxing poster boy b a meth addict c a social media influencer d benjamin button,
 9:a megan thee stallions b fifth harmonies c ty dolla signs d natasha lyonnes, 10 a one hundred b sixty c eighty d fourty, 11 a cover-up b discount double check c inadequate usage of a first round pick d texans salary cap issues, 
-12:a mimas b astrolabe c astra d rhea, 13: a d b k c special k d c,14: a mdx b crx c mdrx d rlx,15:a it isnt a string b you cant mention a number if youre in num pi c it is not any metric unit d np in pi is different In geomtry than In coding, 
+12:a mimas b astrolabe c astra d rhea, 13: a d b k c special k d c,14: a mdx b crx c mdrx d rlx,15:a it isnt a string b you cant mention a number if youre in num pi c it is not any metric unit d np in pi is different in geomtry than in coding, 
 16:a venusian b martian c neptunian d uranian,17:a bubble b covid playoff c can jam tournament d covid election 18: a brain chemicals b immunity c trump derangement syndrome d covid risk, 
 19: a negative twenty b fifteen c twenty d twenty-two,20:a wealth b fame c power d belief in god,21:a twice b half c four times d just]
 covidstrings2mathproblemlist=[if aaron rodgers decided 2 weeks later that he would get a johnson and johnson vaccine how soon after getting vaccinated can he play?,the local flag day in town has been postponed due to an outbreak of the variant omicron if the guidelines say that the positivity rate can be not greater than six and a quarter percent and the rate is six and four ninths percent can the event happen on the new date of june seventeenth?, if former president trump shared fourteen covid misinformation posts over a period of two days assuming they are divided equally, how many posts per day did trump share?, if the same town decided to have a juneteenth celebration and the same guidelines regarding positivity were used could juneteenth be held if the rate was five and one fiftieth percent?, if a study found that immunization increases the likelihood that one would decline vaccination by sixty-five percent what could get done to convince skeptical people? ,this same study found that when asked about what party they would vote for in the midterms seventy one percent of people who were immunized would vote republican whereas eighty percent of people who say they were vaccinated vote democrat if the margin of error is two and nine tenths percent what is the range?, if+it+becomes+public+that+vitamin+d+actually+only+has+a+third+of+the+efficacy+of+vaccines+should+the+store+offer+vitamin+d?,the+same+poll+showed+that+if+a+New+variant+came+support+would+increase+by+three+percent+what+is+the+new+range?,the+same+poll+showed+that+once+the+variant+leaves+support+recedes+by+five+percent+what+is+the+new+range?]
-covidstrings2sentencelist=[trump+said+covid+made+him+twenty+years+younger,vitamin d exposure is one way to boost immunity ,work from home is not just For famous people,covid+misninformation+spreads+just+as+fast+as+covid+itself
+covidstrings2sentencelist=[trump+said+covid+made+him+twenty+years+younger,vitamin d exposure is one way to boost immunity ,work from home is not just for famous people,covid+misninformation+spreads+just+as+fast+as+covid+itself
 string.split   
 arrays.aslist(list<string>)
 for(string ss:arr){
