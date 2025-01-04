2d9905a785dbf308c5fefcad6963b2435675b949
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-01-03 22:16:37 -0500

    Update Covid Strings 2 second round .js
    will edit changes to microsoft doc.

--- Covid Strings 2 second round .js
+++ Covid Strings 2 second round .js
@@ -1,28 +1,27 @@
-doc.type HTML
+doc.type html
 script
-  html
+html
 head
 utf8
-viewport content=width=device-width, initial-scale=one>
+viewport content=width=device-width, initial-scale=one
 title covidstrings 2 second round 
 filename=covidstrings2 second round .js
-highest remaining seed =(highestremaining num value)
-lowest remaining seed = (lowestremaining num value )
-intermediateSeed1=(first middle remaining value in covid strings list)
-intermediateSeed2=(second middle remaining value in covid strings list)
-console.log(covid strings2 second round)
-create matchup=(intermediateseed2 versus intermediate seed1)
-matchup = document.createelement(div);
+highest remaining seed (highest remaining num value)
+lowest remaining seed (lowest remaining num value )
+intermediateseed1=(first middle remaining value in covid strings list)
+intermediateseed2=(second middle remaining value in covid strings list)
+create matchup(intermediate seed2 versus intermediate seed1)
+matchup=document.createelement(div);
 matchup.classlist.add(matchup);
 matchup.innerHTML = <div>${team1} ${team2}
 return matchup;
 create matchup=(lowest remaining seed Versus Highest Remaining Seed )
 matchup =document.createelement(div);
-matchup.ClassList.add(matchup);
-matchup.innerHTML = <Div>${team1}</Div><Div>Vs</Div><Div>${team2}</Div>
-java util.Scanner
-java.Util
-questions[]Questions={
+matchup.classlist.add(matchup);
+matchup.innerHTML = <div>${team1}${team2}</div>
+java util.scanner
+java.util
+questions[]questions={
 question18
 question19
 question20
