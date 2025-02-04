33f015f3340219fc06c4a147dcd4c8afeda343cf
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-02-03 19:01:50 -0500

    Update Covid Strings 2 second round .js
    done.

--- Covid Strings 2 second round .js
+++ Covid Strings 2 second round .js
@@ -17,12 +17,12 @@ highest remaining seed (highest remaining num value)
 lowest remaining seed (lowest remaining num value )
 intermediateseed1=(first middle remaining value in covid strings list)
 intermediateseed2=(second middle remaining value in covid strings list)
-create matchup(intermediate seed2 versus intermediate seed1)
+matchup1=(intermediate seed2 versus intermediate seed1)
 matchup=document.createelement(div);
 matchup.classlist.add(matchup);
 matchup.innerhtml = <div>${team1}/${team2}
 return matchup;
-create matchup=(lowest remaining seed versus highest remaining Seed )
+matchup2=(lowest remaining seed versus highest remaining seed )
 matchup=document.createelement(div);
 matchup.classlist.add(matchup);
 matchup.innerhtml = <div>${team1}/${team2}
@@ -43,17 +43,14 @@ char[] chars = char[numberofblanks]
 arrays.fill chars 
 return string chars 
 addeventlistener
-if event.target.classlist.contains(team)
-result =declarewinner(team1score,team2score);
-advancingteams=currentteams.slice(currentteams)
-if intermediate seed1 score>=fourty:
+advancingteams=slice(currentteams)
+result=declarewinner(matchup1)
+if intermediate seed1 score>=forty:
 console.log(intermediate seed1 wins)
 else:
 console.log(intermediate seed2 wins)
-if event.target.classlist.contains(team)
-result = declarewinner(team1score,team2score);
-advancingteams=slice(currentteams)
-if highest remaining seed score>=fourty:
+result =declarewinner(matchup2)
+if highest remaining seed score>=forty:
 console.log (highest remaining seed wins)
 else:
 console.log(lowest remaining seed wins)
