5db9e8f176c6694e57ceff8ce16b244070ccf878
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-05-20 18:07:34 -0400

    Update Covid Strings 2 background.js
    will edit heading changes into read me later.

--- Covid Strings 2 background.js
+++ Covid Strings 2 background.js
@@ -1,5 +1,14 @@
 doc.type.html
 script
+<header.html>
+heading variables 
+the heading numbers that determine where each heading and subheading go 
+heading1=#background overview
+heading2=#functions
+heading3=background code
+<header.html>
+background overview
+background for covid strings2
 header=document create.element(header)
 header.style.backgroundcolor=#4caf50
 header.style.color=’white’
@@ -7,50 +16,50 @@ header.style.padding=’10Px’
 header.style.textalign=’center’
 header.style.text.font size=’24Px’
 header.text content=’covid strings 2’ 
-document AppendChild(Header)
+document appendChild(header)
 heading1=#background overview
 heading2=#functions
 heading3=background code
 <header.html>
 background overview
 background for covid strings 2
-This. Img=document.createElement(Covid)
-Img.Src=Http://Www.Google.Com/Intl/En_com/Images/Logo_plain.Png
-Src=Document GetElementByID(Header)
-Src.AppendChild(Img)
+this.img=document.createelement(covid)
+img.src=http://www.google.com/intl/en_com/images/logo_plain.png
+src=document getelementbyiD(header)
+src.appendchild(img)
 <div id=header></div>
 audio=newaudio(audio_file mp3)
 audio.play 
 random button
 button (set random color)
-option button=
+option button
 button (change color)
 <header.html>
 functions
 functions used in covid strings2
 public fun graphics(){
-jpanel color panel=j panel();
+color panel=j panel();
 color(panel,center)
-jpanel button panel=
+button panel
 jpanel (flowlayout(flowlayout.center)
 buttonpanel.add(randombutton);
 random.button add actionListener(actionlistener)
 override 
-public actionperformed(actionevent e){
+public actionperformed(actionevent e)
 random r= newrandom();
 colorpanel.setbackground((rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))
 buttonpanel.add(optionbutton);
 optionbutton.add actionlistener(actionlistener)
 override 
 public actionperformed(actionevent e)
-<Header.Html>
-Background Code 
-Code To Program Game Background
+<header.html>
+background code 
+code to program covid strings 2 background 
 color multi=jcolorchooser.showdialog(funwithgraphics.this,))
 if(multi!=null)
 color panel background(m)
 add(buttonpanel,borderlayout.south)
-public static main(string [] args){
+public static main(string [] args)
 jframe f = covidstrings2()
 f.set title(covid strings2)
 f.setcloseoperation(exit_on_close);
@@ -70,7 +79,7 @@ else:
 score-=twenty
 lblscore=document.getelementbyid(lblscore)
 lbl.score.innerhtml=score
-private j button next button=jbutton(next)
+next button=jbutton(next)
 nextbutton add.actionlistener
 public actionperformed(action event e) 
 system.out print.in (next button clicked)
