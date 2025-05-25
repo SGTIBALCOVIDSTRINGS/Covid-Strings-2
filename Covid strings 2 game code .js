3c18de30309cf8304f87deceab97f18a7cd44f69
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-05-25 11:45:53 -0400

    Update Covid strings 2 game code .js
    will add changes to read me.

--- Covid strings 2 game code .js
+++ Covid strings 2 game code .js
@@ -1,8 +1,35 @@
 doc.type.html
+html lang en
 script
+header=document create.element(header)
+header.style.backgroundcolor=#4caf50
+header.style.color=’white’
+header.style.padding=’10px’
+header.style.textalign=’center’
+header.style.text.fontsize=’24px’
+header.textcontent=’welcome to covid strings2’
+document appendchild(header)
+<header.html>
+heading variables 
+the heading numbers that determine where each heading and subheading go 
+heading1=game code overview
+heading2=background functions
+heading3= game code part1
+heading4=phrases
+heading5=math problems
+heading6=sentences
+heading7=multiple choice questions
+heading8=game code part2 
+heading9=answer checker
+heading10=winner checker
+subheading1=polling problems
+subheading2= answer scanner
 imagesrc =www.google.com/intl/en_com/images/logo_plain.png;
 src = document.getelementbyid(covid virus)
 filename=covidstrings 2.js 
+<header.html>
+game code overview
+an outline of the game code for covid strings2
 get element by id
 appendchild
 java.awt graphics
@@ -16,12 +43,15 @@ public fun graphics(){
 color panel=j panel();
 add (panel,center)
 button panel
-j panel (flowlayout)
+new panel (flowlayout)(new flowlayout(flowlayout.center)
 buttonpanel.add(randombutton);
 random.button add actionlistener(actionlistener)
 override 
-publicactionperformed(actionevent e){
+publicactionperformed(actionevent e)
 random r= newrandom();
+<header.html>
+background functions
+the functions that lead into the background that are the first things the user will experience
 colorpanel.setbackground(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))
 num teams
 total players=players per team
@@ -33,15 +63,15 @@ teamsgrouping=(rnextint8)(rnextint8)(rnextint8)(rnextint8)(rnextint8)(rnextint8)
 buttonpanel.add(optionbutton);
 optionbutton.add actionlistener(action listener)
 override 
-publicactionperformed(actionevente){
+publicactionperformed(actionevente)
 color multi=jcolorchooser.showdialog(funwithgraphics.this,)
 if(multi!=null)
 color panel set.background(m)
 add(buttonpanel, borderlayout.south)
-public static  main(string [] args){
+public static  main(string [] args)
 jframe f = covidstrings2()
 f.set title(covidstrings2)
-f.setcloseoperation(jframe.exit_on_close);
+f.setcloseoperation(exit_on_close);
 f.setsize
 f.set visible(true);
 <label id=lblscore>zero</label>
@@ -68,23 +98,88 @@ object {a:one,b:two,c:three,d:four,e:five,f:six,g:seven,h:eight}
 keys object keys(team)
 entries object entries(obj);
 value object= [teamcolor]
+<header.html>
+game code part1
+part1 of game code for covid strings2(value object[team-color]-desired color green)
 covid strings2 team list=[orange rna messengers,pink covid disease fighters,red protein spikers ,white transit angels,blue curve crushers ,magenta magnetizers ,black night riders,purple brave hearts]
 covid strings2 debugging question=(whats wrong with this code "the"+"covid"+"death"+"toll"+"in"+"terms"+"of"+"pi"+"is"+"one"+"thousand") 
+string manager{non-concatenated}
+insert string (entity,string)
+entity.element.text=string
+string manager insert string
+string manager{concatenated}
+insert string(entity,string)
+entity.element.text=string
+string manager insert string
+string=””
+for string in covid strings2 phrase list 
+select characters
+edit diffuse property
+desired color=green
+for string in covid strings2 math problems list
+select characters
+edit diffuse property
+desired color=green
+for string in covid strings2 sentence list 
+select characters
+edit diffuse property
+desired color=green
+<header.html>
+phrases
+phrases that involve both non-concatenated and concatenated strings 
 covid strings2 phrase list=[non-concatenated,"no sun cubed does not kill viruses", "elon musk is wrong take the purple pill", "nothing labeled dexie turns you into benjamin button," "i am not a superspreader i am a supraspreader" "mdx does not mean medical doctors prescription","astrazeneca is only slightly better than the saturn astra", "talk about staying at home lasts shorter than a soda can on venus" "aaron rodgers proves the coverup is truly worse than the crime"
 concatenated,"no"+"corona"+"beer"+"has"+"nothing"+"to"+"do"+"with"+"corona"+"virus","work"+"from"+"home"+"is"+"not"+"just"+"for"+"rihanna","it"+"is"+"also"+"not"+"just"+"fifth"+"harmonys"+"right"+"either","vitamin"+"d"+"however"+"is"+"a"+"compliment"+"to+"getting"+"vaccinated","at"+"least"+"sixty"+"minutes"+"of"+"exercise"+"also"+"helps","no"+"covid"+"was"+"not"+"cheated"+"out"+"of"+"the"+"twenty-twenty"+"election","one"+"comedian"+"believed"+"the"+"last"+"year"+"was"+"one"+"and"+"four"+"twentyfirsts"+"of"+"a"+"uranian"+"year"
 "it"+"figures"+"a"+"bubble"+"would"+"employ"+"a"+"round"+"robin"+"format"]
+<header.html>
+multiple choice questions
+multiple choice questions and answers that will appear in covid strings2
 covidstringsmultiplechoicelist2=[1: a shorter b longer c a reflection of d the same , 2:a cheated b arrested c stolen d expelled ,3:a people with autism b nfl employees c rihanna d social media influencers, 
 4: a news b supra c ymca d misinformation,5:a supersized b cubed c squared d blocked, 6: a beer b cerveza c solar flare d cider, 7:a blue b purple c red d orange, 8: a doxing poster boy b a meth addict c a social media influencer d benjamin button,
 9:a megan thee stallions b fifth harmonies c ty dolla signs d natasha lyonnes, 10 a one hundred b sixty c eighty d fourty, 11 a cover-up b discount double check c inadequate usage of a first round pick d texans salary cap issues, 
 12:a mimas b astrolabe c astra d rhea, 13: a d b k c special k d c,14: a mdx b crx c mdrx d rlx,15:a it isnt a string b you cant mention a number if youre in num pi c it is not any metric unit d np in pi is different in geomtry than in coding, 
 16:a venusian b martian c neptunian d uranian,17:a bubble b covid playoff c can jam tournament d covid election 18: a brain chemicals b immunity c trump derangement syndrome d covid risk, 
 19: a negative twenty b fifteen c twenty d twenty-two,20:a wealth b fame c power d belief in god,21:a twice b half c four times d just]
+<header.html>
+math problems
+covid related math problems that will appear in covid strings2
 covidstrings2mathproblemlist=[ non-concatenated "if aaron rodgers decided 2 weeks later that he would get a johnson and johnson vaccine how soon after getting vaccinated can he play"?,"the local flag day in town has been postponed due to an outbreak of the variant omicron if the guidelines say that the positivity rate can be not greater than six and a quarter percent and the rate is six and four ninths percent can the event happen on the new date of june seventeenth"?, "if former president trump shared fourteen covid misinformation posts over a period of two days assuming they are divided equally, how many posts per day did trump share"?, "if the same town decided to have a juneteenth celebration and the same guidelines regarding positivity were used could juneteenth be held if the rate was five and one fiftieth percent"?, "if a study found that immunization increases the likelihood that one would decline vaccination by sixty-five percent what could get done to convince skeptical people"? ,"this same study found that when asked about what party they would vote for in the midterms seventy one percent of people who were immunized would vote republican whereas eighty percent of people who say they were vaccinated vote democrat if the margin of error is two and nine tenths percent what is the range"?, 
 concatenated"if"+"it"+"becomes"+"public"+"that"+"vitamin"+"d"+"actually"+"only"+"has"+"a"+"third"+"of"+"the"+"efficacy"+"of"+"vaccines"+"should"+"the"+"store"+"offer"+"vitamin"+"d"?,"if"+"the"+"same"+"poll"+"showed"+"that"+"if"+"a"+"new"+"variant"+"came"+"support"+"would"+"increase"+"by"+"three"+"percent"+"what"+"is"+"the"+"new"+"range"?,"if"+"the"+"same"+"poll"+"showed"+"that"+"once"+"the"+"variant"+"leaves"+"support"+"recedes"+"by"+"five"+"percent"+"what"+"is"+"the"+"new"+"range"?]
+public static main(string []args)
+int number
+number=number*2
+system.out print in (“the number is“+number)
+<subheading.html>
+polling problems
+math problems that involve the use of polls
+polling function (exact,number)
+console log (exact:${exact}, number:${number},
+if polling function log== (exact,number)
+return true
+else:
+return false
+if polling function log==(number,exact)
+return false
+else:
+return true  
+for math problems in covid strings math problem list 
+if math problem==polling percent:
+subtract exact from100
+else:
+remain 
+if math problem==margin of error:
+add and subtract exact number 
+else:
+remain 
+<header.html>
+sentences
+covid related sentences that will appear in covid strings2
 covidstrings2sentencelist=["trump"+"said"+"covid"+"made"+"him"+"twenty"+"years"+"younger","vitamin d exposure is one way to boost immunity","work from home is not just for famous people","covid"+"misninformation"+"spreads"+"just"+"as"+"fast"+"as"+"covid"+"itself"
+<header.html>
+game code part2
+the rest of the game code excluding answer and winner checker
 string.split   
 arrays.aslist(list<string>)
-for(string ss:arr){
+for(string ss:arr)
 system.outprintin(ss)
 java util.scanner
 java.util
@@ -119,10 +214,33 @@ else:
 select from multiple choice
 importsyntax 
 substring(start,end)
+<header.html>
+answer checker
+the feature within covid strings that allows the software to check whether a users answer is correct
 if selectedanswer==correct answer:
 log true
 else:
 log false
+if selectedanswer==incorrect answer:
+return false
+else:
+return true
+for math problems in covid strings
+if typed answer ==correct
+return true
+else:
+return false
+if typed answer== not correct
+return false
+else
+return true
+public static main(string []args)
+int number
+number=number*2
+system.out print in (“the number is“ +number)
+<subheading.html>
+answer scanner
+the feature within the covid strings answer checker that allows for confirmation of the user 
 answer checker
 scanner=new scanner(system.in)
 string correct answer
@@ -132,6 +250,9 @@ system.out.print.in(correct)
 else:
 system.out.printin(incorrect)
 scanner.close
+<header.html>
+winner checker
+the feature within covid strings that allows for the player to know if their team won 
 check winner:
 if winner==true:
 log(the winner is)
@@ -140,5 +261,9 @@ log(no winner yet)
 if users team wins covid strings2
 console log users team wins covid strings2 
 else
+remain
+if users team loses covid strings2:
 return game over  
+else
+remain
 return filename 
